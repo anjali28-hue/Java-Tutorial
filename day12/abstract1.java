1a3fddd24cc2902568f195dca48c913da8389e56
anjali28-hue <anjalianjalimanohar@gmail.com>
2026-07-06 20:10:21 +0530

    Add files via upload

---
+++ day12/abstract1.java
@@ -0,0 +1,25 @@
+package day12;
+abstract class computer {
+    abstract void display();
+}
+class laptop extends computer{
+    void display() {
+        System.out.println("laptop");
+    }
+}
+class desktop extends computer {
+    void display() {
+        System.out.println("Desktop");
+    }
+}
+public class abstract1 {
+    public static void main(String args[]){
+        computer sc=new laptop();
+        computer sc1=new desktop();
+        sc.display();
+        sc1.display();
+    }
+
+}
+
+
